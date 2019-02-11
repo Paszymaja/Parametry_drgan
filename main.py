4c09154e2e5daaa8be50aa22f56b9003c9ae046e
Paszymaja <sprzepiora@hotmail.co.uk>
2019-02-11 09:45:00 +0100

    Działa

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def wczytanie_danych(sheet):
     mat = np.array(tab).reshape(60, 7)
     return mat
 
+
 def wczytanie_stanowisk(sheet):
     tab = []
     for row_i in range(2, 5):
@@ -24,35 +25,57 @@ def wczytanie_stanowisk(sheet):
     return mat
 
 
-def macierz_A(macierz, macierz_stanowisk):
+def macierz_a(macierz, macierz_stanowisk):
     tab = []
-    print(macierz)
+    value = lambda a, b: np.sqrt(pow(macierz[i][3] - macierz_stanowisk[a][b], 2) +
+                                 pow(macierz[i][4] - macierz_stanowisk[a][b+1], 2) +
+                                 pow(500, 2))
     for i in range(60):
         for j in range(4):
             if j == 0:
                 tab.append(np.log10(macierz[i][2]))
             elif j == 1:
                 if macierz[i][0] == 1:
-                    tab.append(np.log10(np.sqrt(pow(macierz[i][3] - macierz_stanowisk[0][1], 2) +
-                                                pow(macierz[i][4] - macierz_stanowisk[0][2], 2) +
-                                                pow(500, 2))))
+                    tab.append(np.log10(value(0, 1)))
                 elif macierz[i][0] == 2:
-                    tab.append(np.log10(np.sqrt(pow(macierz[i][3] - macierz_stanowisk[1][1], 2) +
-                                                pow(macierz[i][4] - macierz_stanowisk[1][2], 2) +
-                                                pow(500, 2))))
+                    tab.append(np.log10(value(1, 1)))
                 elif macierz[i][0] == 3:
-                    tab.append(np.log10(np.sqrt(pow(macierz[i][3] - macierz_stanowisk[2][1], 2) +
-                                                pow(macierz[i][4] - macierz_stanowisk[2][2], 2) +
-                                                pow(500, 2))))
+                    tab.append(np.log10(value(2, 1)))
             elif j == 2:
                 if macierz[i][0] == 1:
-                    tab.append(np.sqrt(pow(macierz[i][3] - macierz_stanowisk[0][1], 2)))
-    return tab
+                    tab.append(value(0, 1))
+                elif macierz[i][0] == 2:
+                    tab.append(value(1, 1))
+                elif macierz[i][0] == 3:
+                    tab.append(value(2, 1))
+            elif j == 3:
+                tab.append(1)
+    mat = np.array(tab).reshape(60, 4)
+    return mat
+
+
+def macierz_y(macierz):
+    tab = []
+    for i in range(60):
+        for j in range(1):
+            tab.append(np.log10(macierz[i][5]))
+    mat = np.array(tab).reshape(60, 1)
+    return mat
 
 
 macierz_danych = wczytanie_danych(arkusz)
-darek = wczytanie_stanowisk(arkusz)
-print(macierz_A(macierz_danych, darek))
+stanowiska = wczytanie_stanowisk(arkusz)
+
+macierzA = macierz_a(macierz_danych, stanowiska)
+macierzY = macierz_y(macierz_danych)
+
+macierzT = macierzA.transpose()
+macierzA = np.dot(macierzT, macierzA)
+macierzY = np.dot(macierzT, macierzY)
+
+wynik = np.linalg.solve(macierzA, macierzY)
+print(wynik)
+
 
 
 
