74c7f0811a4505b3bdfada73f7d9aab0041c452a
Paszymaja <sprzepiora@hotmail.co.uk>
2019-02-11 11:33:47 +0100

    Sigma i tede

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ wb = openpyxl.load_workbook('dane.xlsx')
 arkusz = wb['Arkusz1']
 
 
+
 def wczytanie_danych(sheet):
     tab = []
     for row_i in range(2, 62):
@@ -63,18 +64,69 @@ def macierz_y(macierz):
     return mat
 
 
+def odleglosci_epicentralne():
+    tab = []
+    a = 0
+    for i in range(4):
+        tab.append(a)
+        a = a + 500
+    return tab
+
+
+def macierz_y_prog(macierz, wyniki):
+    tab = []
+    for i in range(60):
+        for j in range(1):
+            tab.append((wyniki[0] * macierz[i][0])+(wyniki[1]*macierz[i][1]) + (wyniki[2]*macierz[i][2]) + wyniki[3])
+    mat = np.array(tab).reshape(60, 1)
+    return mat
+
+
+def odchylenie_standardowe(macierz, macierz_prog):
+    tab = []
+    for i in range(60):
+        tab.append(macierz[i][0]+macierz_prog[i][0])
+    mat = np.array(tab).reshape(60, 1)
+    return mat
+
+
+def sigma(odchylenie, odchylenie_srednia):
+    a = 0
+    for i in range(60):
+        a = a + pow(odchylenie[i] - odchylenie_srednia, 2)
+    a = np.sqrt(a)/59
+    return a
+
+
 macierz_danych = wczytanie_danych(arkusz)
 stanowiska = wczytanie_stanowisk(arkusz)
+odl_ep = odleglosci_epicentralne()
 
 macierzA = macierz_a(macierz_danych, stanowiska)
 macierzY = macierz_y(macierz_danych)
 
 macierzT = macierzA.transpose()
-macierzA = np.dot(macierzT, macierzA)
-macierzY = np.dot(macierzT, macierzY)
+macierzA_mnozona = np.dot(macierzT, macierzA)
+macierzY_mnozona = np.dot(macierzT, macierzY)
 
-wynik = np.linalg.solve(macierzA, macierzY)
+wynik = np.linalg.solve(macierzA_mnozona, macierzY_mnozona)
+macierzY_prog = macierz_y_prog(macierzA, wynik)
+odchylenie_stnd = odchylenie_standardowe(macierzY, macierzY_prog)
+odchylenie_stnd_srednia = sum(odchylenie_stnd)/60
+sigma_wynik = sigma(odchylenie_stnd, odchylenie_stnd_srednia)
+
+
+print(sigma_wynik)
+print(odchylenie_stnd_srednia)
+
 print(wynik)
+print()
+
+
+
+
+
+
 
 
 
